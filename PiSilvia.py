edbbd31d7b28082b5476d4a87bb6b6ae691926ef
Thijs <thijs.franssen@kpn.com>
2020-03-17 13:52:48 +0100

    Changes

--- PiSilvia.py
+++ PiSilvia.py
@@ -2,8 +2,16 @@ import MAX6675.MAX6675 as MAX6675
 import Adafruit_GPIO.SPI as SPI
 import RPi.GPIO as GPIO  # Import Raspberry Pi GPIO library
 
+#Initialise Thermocouple and average temperature readings
 SPI_PORT = 0
 SPI_DEVICE = 0
 sensor = MAX6675.MAX6675(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+temphist = [0., 0., 0., 0., 0.]
+self.i = 0
+
+
 temperature = sensor.readTempC()
 print 'Thermocouple Temperature: {0:0.3F}C'.format(temperature)
+temphist[self.i % 5] = temperature
+avgtemp = sum(temphist) / len(temphist)
+print(avgtemp)
