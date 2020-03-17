c57cd9018dde786c689a00fb1737b144734a1305
Thijs <thijs.franssen@kpn.com>
2020-03-17 13:46:55 +0100

    Update

--- PiSilvia.py
+++ PiSilvia.py
@@ -1,31 +1,8 @@
-import time
-
+import MAX6675.MAX6675 as MAX6675
 import Adafruit_GPIO.SPI as SPI
-import MAX6675 as MAX6675
+import RPi.GPIO as GPIO  # Import Raspberry Pi GPIO library
 
-
-# Define a function to convert celsius to fahrenheit.
-def c_to_f(c):
-        return c * 9.0 / 5.0 + 32.0
-
-
-# Uncomment one of the blocks of code below to configure your Pi or BBB to use
-# software or hardware SPI.
-
-# Raspberry Pi software SPI configuration.
-# CLK = 25
-# CS  = 24
-# DO  = 18
-# sensor = MAX6675.MAX6675(CLK, CS, DO)
-
-# Raspberry Pi hardware SPI configuration.
-SPI_PORT   = 0
+SPI_PORT = 0
 SPI_DEVICE = 0
-sensor = MAX6675(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
-
-# Loop printing measurements every second.
-print('Press Ctrl-C to quit.')
-while True:
-	temp = sensor.readTempC()
-	print('Thermocouple Temperature: {0:0.3F}C'.format(temperature))
-	time.sleep(1.0)
+sensor = MAX6675.MAX6675(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+print 'Thermocouple Temperature: {0:0.3F}°C'.format(temperature)
