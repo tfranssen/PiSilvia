ad23c7da3baf906e26832e42ddeb5667c91e3e66
Thijs <thijs.franssen@kpn.com>
2020-03-17 13:16:54 +0100

    Updated SPI

--- PiSilvia.py
+++ PiSilvia.py
@@ -1,14 +1,16 @@
 #!/usr/bin/ python
 import time
 import board
-import busio
+import busio.SPI as SPI
 import digitalio
-import MAX6675.MAX6675 as MAXLIB
+import MAX6675.MAX6675 as MAX6675
 
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 cs = digitalio.DigitalInOut(board.D5)
 
-sensor = MAXLIB(spi, cs)
+SPI_PORT   = 0
+SPI_DEVICE = 0
+sensor = MAX6675.MAX6675(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
 temperature = sensor.readTempC()
 print('Thermocouple Temperature: {0:0.3F}°C'.format(temperature))
